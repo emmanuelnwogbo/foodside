ce75135e669c5391fe224b6e2e67f863068954cc
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-07-15 10:04:14 +0100

    added cooking duration sort for recipes

--- src/components/LeftContainer.jsx
+++ src/components/LeftContainer.jsx
@@ -17,21 +17,23 @@ class LeftContainer extends Component {
       //console.log('recipes exist ffs', this.props.state.recipes)
       if (this.props.state.recipes.length > 0) {
         return this.props.state.recipes.map(recipe => {
-          recipe.time_to_prepare = Math.floor(Math.random()*(45-25+1)+25);
-          return (
-            <Suspense key={recipe.recipe_id} fallback={<div></div>}>
-              <Recipe 
-                image={recipe.image_url}
-                social_rank={recipe.social_rank}
-                title={recipe.title}
-                publisher={recipe.publisher}
-                publisher_url={recipe.publisher_url}
-                id={recipe.recipe_id}
-                f2f_url={recipe.f2f_url}
-                source_url={recipe.source_url}
-                time_to_prepare={recipe.time_to_prepare}/>
-            </Suspense>
-          )
+          recipe.time_to_prepare = `${Math.floor(Math.random()*(45-25+1)+25)}`;
+          if (recipe.time_to_prepare <= this.props.state.cookingDuration) {
+            return (
+              <Suspense key={recipe.recipe_id} fallback={<div></div>}>
+                <Recipe 
+                  image={recipe.image_url}
+                  social_rank={recipe.social_rank}
+                  title={recipe.title}
+                  publisher={recipe.publisher}
+                  publisher_url={recipe.publisher_url}
+                  id={recipe.recipe_id}
+                  f2f_url={recipe.f2f_url}
+                  source_url={recipe.source_url}
+                  time_to_prepare={recipe.time_to_prepare}/>
+              </Suspense>
+            )
+          }
         })
       }
 
